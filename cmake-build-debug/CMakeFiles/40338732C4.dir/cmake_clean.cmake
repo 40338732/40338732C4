f40d6f3ecea9a7f23e1f86f95ad922c7b0cfd2c5
40338732 <46718212+40338732@users.noreply.github.com>
2020-04-22 18:48:11 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/40338732C4.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "40338732C4.exe"
+  "40338732C4.exe.manifest"
+  "40338732C4.pdb"
+  "CMakeFiles/40338732C4.dir/main.c.obj"
+  "lib40338732C4.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/40338732C4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
